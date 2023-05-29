951de3b40ea12682fa38aedb58df8e6731701d65
Alexander Meshcheryakov <98259688+Anudiss@users.noreply.github.com>
2023-05-29 21:38:17 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,121 @@
+const reviews = [
+    {
+        "avatar": "../images/logo.png",
+        "name": "Веник",
+        "text": "ОФЫОвфывоцвфыв",
+        "evaluation": 5
+    },
+    {
+        "avatar": "../images/logo.png",
+        "name": "Веник",
+        "text": "ОФЫОвфывоцвфыв laksdl alskd asldkawlkd jask dajsl djalksjd alkwjd laksd j ajslkd jaslkj",
+        "evaluation": 4
+    },
+    {
+        "avatar": "../images/logo.png",
+        "name": "Веник",
+        "text": "ОФЫОвфывоцвфыв",
+        "evaluation": 3
+    },
+    {
+        "avatar": "../images/logo.png",
+        "name": "Веник",
+        "text": "ОФЫОвфывоцвфыв",
+        "evaluation": 2
+    },
+    {
+        "avatar": "../images/logo.png",
+        "name": "Веник",
+        "text": "ОФЫОвфывоцвфыв",
+        "evaluation": 1
+    },
+    {
+        "avatar": "../images/logo.png",
+        "name": "Веник",
+        "text": "ОФЫОвфывоцвфыв",
+        "evaluation": 0
+    }
+]
+
+$(document).ready(function () {
+    LoadReviews();
+
+    const reviews__container = $('.reviews__container');
+    const review = $('.review');
+    
+    var scrollValue = () => { return reviews__container.width(); }
+    var maxScroll = () => { return scrollValue() * ((review.length + review.length % 2) / ($(document).width() > 768 ? 2 : 1) - 1); }
+    const scrollTime = 300;
+    var scroll = 0;
+
+    $('.arrow.left').bind('click', (e) => ScrollLeft());
+
+    $('.arrow.right').bind('click', (e) => ScrollRight());
+
+    $(window).bind('resize', function(e) {
+        
+        scroll -= scroll % scrollValue();
+    });
+
+    function ScrollLeft() {
+        scroll = reviews__container[0].scrollLeft;
+        if (scroll == 0) 
+            return;
+        scroll -= scrollValue();
+        scroll = Clamp(0, maxScroll(), scroll);
+        reviews__container.animate({scrollLeft:scroll}, scrollTime, 'linear');
+    }
+    
+    function ScrollRight() {
+        scroll = reviews__container[0].scrollLeft;
+        if (scroll == maxScroll())
+            return;
+        scroll += scrollValue();
+        scroll = Clamp(0, maxScroll(), scroll);
+        reviews__container.animate({scrollLeft:scroll}, scrollTime, 'linear');
+    }
+
+    $('.scrollUp').bind('click', function(e) {
+        $('html').animate({scrollTop:0}, 300);
+        console.log(e);
+    });
+
+});
+
+function LoadReviews()
+{
+    reviews.forEach(review => {
+        $('.reviews__container').append(`<div class="review">
+                                        <div class="inner__review">
+                                            <div class="avatar"><img src="${review['avatar']}" alt=""></div>
+                                            <div class="name">${review['name']}</div>
+                                            <div class="evaluation">${LoadStars(review.evaluation)}</div>
+                                            <div class="text">${review['text']}</div>
+                                        </div>
+                                    </div>`)
+    });
+}
+
+function LoadStars(count)
+{
+    const enabled = 'yellow';
+    const disabled = 'lightgray';
+    return $.map(Array.from({length:5}), function(e, i) {
+        return `<i class="fa fa-star fa-2x" style="color: var(--${i < count ? enabled : disabled});" aria-hidden="true"></i>`;
+    }).join('');
+}
+
+function Clamp(min, max, value)
+{
+    return Max(min, Min(max, value));
+}
+
+function Min(v1, v2)
+{
+    return v1 > v2 ? v2 : v1;
+}
+
+function Max(v1, v2)
+{
+    return v1 < v2 ? v2 : v1;
+}
---
+++ scripts/services.js
@@ -0,0 +1,45 @@
+const services =
+[
+    {
+        'icon': '../images/дипломная.png',
+        'name': 'Дипломная работа',
+        'time': '1-2 недели',
+        'price': '5 000'
+    },
+    {
+        'icon': '../images/курсовая.png',
+        'name': 'Курсовая работа',
+        'time': '1 недели',
+        'price': '2 000'
+    },
+    {
+        'icon': '../images/реферат.png',
+        'name': 'Реферат',
+        'time': '3 часов',
+        'price': '1 000'
+    },
+    {
+        'icon': '../images/презентация.png',
+        'name': 'Презентация',
+        'time': '5 часов',
+        'price': '1 000'
+    },
+    {
+        'icon': '../images/антиплагиат.png',
+        'name': 'Антиплагиат',
+        'time': '3 часов',
+        'price': '100'
+    }
+]
+
+$(document).ready(function()
+{
+    $('.inner__services').append(
+        $.map(services, (service) => { return `<div class="service">
+                                                   <div class="icon"><img src="${service.icon}" alt=""></div>
+                                                   <span class="name">${service.name}</span>
+                                                   <div class="time">от ${service.time}</div>
+                                                   <div class="price">от ${service.price} ₽</div>
+                                               </div>` }).join('')
+    );
+});
